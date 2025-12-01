b7d7bb2e97c8874cc177de2e0447f9f2bb76cf24
Wiktoria Kosek <kosek.wiktoria1@gmail.com>
2025-12-01 21:34:37 +0100

    Fix issue with ScannerController

--- Routing.php
+++ Routing.php
@@ -3,7 +3,7 @@
 require_once 'src/controllers/SecurityController.php';
 require_once 'src/controllers/DashboardController.php';
 require_once 'src/controllers/CommunityController.php';
-
+require_once 'src/controllers/ScannerController.php';
 
 class Routing {
     private static $instance = null;
